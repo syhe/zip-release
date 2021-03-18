849170d60e696ee06724b618d8643b917ef4361c
Sýlvan Heuser <Sylvan.Heuser@flexoptix.net>
2021-03-18 14:45:57 +0100

    Use descriptive variable names
    Also fix formatting using `shfmt` and change "invalid" to "unsupported".

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,31 +3,47 @@
 # Create archive or exit if command fails
 set -eu
 
-printf "\n📦 Creating %s archive...\n" "$5"
+ERRORMSG="\n⛔ Unable to create %s archive.\n"
 
-if [ "$3" != "." ]
-then
-  cd $3
+filename="${1}"
+path="${2}"
+directory="${3}"
+exclusions="${4}"
+archive_type="${5}"
+
+printf "\n📦 Creating %s archive...\n" "$archive_type"
+
+if [ "$directory" != "." ]; then
+  cd "$directory"
 fi
 
-if [ "$5" = "zip" ]
-then
-  if [ -z "$4" ]
-  then
-    zip -r $1 $2 || { printf "\n⛔ Unable to create %s archive.\n" "$5"; exit 1;  }
+if [ "$archive_type" = "zip" ]; then
+  if [ -z "$exclusions" ]; then
+    zip -r "$filename" "$path" || {
+      printf "$ERRORMSG" "$archive_type"
+      exit 1
+    }
   else
-    zip -r $1 $2 -x $4 || { printf "\n⛔ Unable to create %s archive.\n" "$5"; exit 1;  }
+    zip -r "$filename" "$path" -x "$exclusions" || {
+      printf "$ERRORMSG" "$archive_type"
+      exit 1
+    }
   fi
-elif [ "$5" = "tar" ]
-then
-  if [ -z "$4" ]
-  then
-    tar -zcvf $1 $2 || { printf "\n⛔ Unable to create %s archive.\n" "$5"; exit 1;  }
+elif [ "$archive_type" = "tar" ]; then
+  if [ -z "$exclusions" ]; then
+    tar -zcvf "$filename" "$path" || {
+      printf "$ERRORMSG" "$archive_type"
+      exit 1
+    }
   else
-    tar -zcvf $1 $2 --exclude=$4 || { printf "\n⛔ Unable to create %s archive.\n" "$5"; exit 1;  }
+    tar -zcvf "$filename" "$path" --exclude="$exclusions" || {
+      printf "$ERRORMSG" "$archive_type"
+      exit 1
+    }
   fi
 else
-  printf "\n⛔ Invalid archiving tool.\n"; exit 1;
+  printf "\n⛔ Unsupported archiving tool.\n"
+  exit 1
 fi
 
-printf "\n✔ Successfully created %s archive.\n" "$5"
+printf "\n✔ Successfully created %s archive.\n" "$archive_type"
